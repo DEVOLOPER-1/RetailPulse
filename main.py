799359eb0e1171c9a91516573af87cb589baa583
Youssef Mohammad <153318445+DEVOLOPER-1@users.noreply.github.com>
2024-12-16 17:25:13 +0200

    ✨ feat(scrapping): add functions to download various retail datasets from Kaggle
    ✨ feat(main): call dataset download functions from scrapping module
    
    ✨ feat(data_processing): initialize data processing module for future enhancements

---
+++ main.py
@@ -0,0 +1,6 @@
+from scrapping import *
+
+download_walmart_2010_2012()
+download_walmart_stock_data_2024()
+download_carrefour_online_sales_2020_2021()
+download_target_market_sales_2016_2018()
---
+++ scrapping.py
@@ -0,0 +1,32 @@
+import kagglehub
+
+def download_walmart_2010_2012():
+    
+    # Download latest version
+    path = kagglehub.dataset_download("saurabhbadole/walmart-super-market-dataset")
+    
+    print("Path to dataset files:", path)
+    
+    
+def download_walmart_stock_data_2024():
+
+    # Download latest version
+    path = kagglehub.dataset_download("umerhaddii/walmart-stock-data-2024")
+    
+    print("Path to dataset files:", path)
+    
+    
+def download_carrefour_online_sales_2020_2021():
+
+    # Download latest version
+    path = kagglehub.dataset_download("sherifhesham89/carrefour-online-sales")
+    
+    print("Path to dataset files:", path)
+    
+    
+def download_target_market_sales_2016_2018():
+
+    # Download latest version
+    path = kagglehub.dataset_download("narendrabariha/e-commerce-target-sales-dataset")
+    
+    print("Path to dataset files:", path)
